5f8e0ed31cd5269885116a223958a75ae88a6e75
Fr0ntcoder <creativ4lk@vk.com>
2026-04-23 19:17:25 +0300

    добавил dto для deals

---
+++ src/module/deals/deals.dto.ts
@@ -0,0 +1,33 @@
+import { DealSource, DealStage } from '@prisma/client'
+import z from 'zod'
+
+export const createDealDto = z.object({
+	title: z.string().min(2),
+	clientId: z.uuid(),
+	stage: z.enum(DealStage),
+	amount: z.coerce.number().nonnegative(),
+	source: z.enum(DealSource),
+	managerId: z.uuid().optional()
+})
+
+export const updateDeadDto = createDealDto
+	.partial()
+	.refine(data => Object.keys(data).length > 0, {
+		message: 'Введите хотя бы одно поле'
+	})
+
+export const listDealDto = z.object({
+	search: z.string().optional(),
+	stage: z.enum(DealStage).optional(),
+	source: z.enum(DealSource).optional(),
+	managerId: z.uuid().optional(),
+	clientId: z.uuid().optional(),
+	dateFrom: z.iso.datetime().optional(),
+	dateTo: z.iso.datetime().optional(),
+	sortBy: z.enum(['createdAt', 'updatedAt', 'amount', 'title']).optional(),
+	sortOrder: z.enum(['asc', 'desc']).optional()
+})
+
+export type CreateDealDto = z.infer<typeof createDealDto>
+export type UpdateDealDto = z.infer<typeof updateDeadDto>
+export type ListDealDto = z.infer<typeof listDealDto>
